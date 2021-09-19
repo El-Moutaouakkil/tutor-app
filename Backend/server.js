c9d46fe1eb267940b7ba7c8947cd698a6f5020bb
lechestercat <chesterortega1991@gmail.com>
2021-09-19 14:39:41 -0400

    - Connected MongoDB Atlas to backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,20 @@
-// server/index.js
+import connectDB from './config/db.js'
+//import studentRoutes from './routes'
+import express from 'express'
+import dotenv  from 'dotenv'
 
-const express = require("express");
-const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
- 
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
-});
+//connect database
+connectDB()
+
+//dotenv config
+dotenv.config()
+
+const app = express()
+
+//Creating API for user
+//app.use('/api/students', studentRoutes)
+
+const PORT = process.env.PORT || 5000
+
+//Express js listen method to run project on http://localhost:5000
+app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
